d1efd29f5db68fd662023fdad2855c37b439a0de
caohao0730 <caohao0730@163.com>
2019-02-12 11:01:19 +0800

    redis pool

--- framework/application/config/redis.php
+++ framework/application/config/redis.php
@@ -3,8 +3,7 @@
 /********************************程序应用配置***************************************/
 $redis_conf['default_master']['host'] = '127.0.0.1';
 $redis_conf['default_master']['port'] = 6379;
-$redis_conf['default_slave']['host'] = '127.0.0.1';
-$redis_conf['default_slave']['port'] = 6380;
+$redis_conf['default_slave']['host'] = '/tmp/redis_pool.sock';
 
 $redis_conf['userinfo']['host'] = '127.0.0.1';
 $redis_conf['userinfo']['port'] = 6379;
